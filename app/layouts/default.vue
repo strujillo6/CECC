ab275fe05e1124a22af3b633b6e235c9db4260c1
Sergio Trujillo del Valle <trujillo.sergio6@gmail.com>
2018-08-25 04:46:15 -0500

    se esctructuro mejor la plantilla por default con flexbox

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
   <v-app class="indigo darken-2">
-    <top-toolbar/>
-     <nuxt/>
-    <bottom-toolbar/>
+    <div >
+      <top-toolbar class="z__index-10 menu-top"/>
+    </div>
+    <div class="content">
+      <nuxt class="main__container"/>
+    </div>
+    <div>
+       <bottom-toolbar class="z__index-10"/> 
+    </div>
   </v-app>
 </template>
 <script>
@@ -15,3 +21,40 @@ export default {
     }
 }
 </script>
+<style lang="scss">
+@import '~/assets/css/tools/_mixin.scss';
+.main__container{
+  z-index: 1;
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: start;
+  width: 100%;
+  height: 100%;
+}
+
+.application--wrap{
+  display: flex;
+  min-height: 100vh;
+  height: 100vh;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  position: relative;
+  width: 100%;
+}
+.z__index-10{
+  z-index: 10;
+}
+.content{
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 1;
+   width: 100%;
+  padding-bottom: 56px;
+  @include breakpoint(tablet){
+    padding-bottom: 0;
+  }
+}
+</style>
