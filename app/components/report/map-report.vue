b219bdd59f27811687b01fd08695dac821d1bbe4
Sergio Trujillo del Valle <trujillo.sergio6@gmail.com>
2018-08-25 04:45:30 -0500

    se crearon los componentes generales para la seccion de reportes

---
+++ app/components/report/map-report.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container__map">
+    <l-map  :zoom=16 :center="[locale.latitude, locale.longitude]">
+      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-marker :lat-lng="[locale.latitude, locale.longitude]">
+        <l-popup>
+          Contenido
+          <v-list-tile-avatar>
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+        </v-list-tile-avatar>
+        </l-popup>
+      </l-marker>
+      <l-geo-json :geojson=bus.geojson ></l-geo-json>
+      <l-control-scale position="bottomleft"></l-control-scale>
+    </l-map>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    bus: {
+      geojson:{
+        "type": "Feature",
+        "properties": {
+            "name": "Mi casa",
+              "amenity": "Baseball Stadium",
+              "popupContent": "This is where the Rockies play!"
+          },
+          "geometry": {
+              "type": "Point",
+              "coordinates": [4.698112, -74.097459]
+      }
+    }}
+  }),
+  props:{
+    locale:{
+      type:Object
+    },
+    marker:{
+      tupe: Object
+    }
+  }
+}
+</script>
+
+<style>
+.leaflet-control-zoom{
+  top: 60px;
+  left: 0;
+}
+.container__map{
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ app/components/report/menu-report.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-toolbar
+      dense
+      floating
+      class="search__map"
+    >
+      <v-text-field
+        hide-details
+        prepend-icon="search"
+        single-line
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>my_location</v-icon>
+      </v-btn>
+
+      <v-btn icon v-on:click="show">
+        <v-icon>{{icon}}</v-icon>
+      </v-btn>
+    </v-toolbar> 
+</template>
+<script>
+export default {
+    props:{
+        icon:{
+            type: String
+        }
+    },
+    methods: {
+        show: function () {
+            this.$emit('show')
+        }
+    }
+}
+</script>
+<style lang="scss">
+@import '~/assets/css/tools/_mixin.scss';
+.search__map{
+  position: absolute;
+  top: 0;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 2;
+  @media (min-width: 335px) {
+    top: 8px;
+  }
+  @include breakpoint(phablet){
+    left: 8px;
+  }
+}
+</style>
